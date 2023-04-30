28f2654d26a70a7dd53a3fd1b770991639e78f36
AlineSto <aline.sto69@gmail.com>
2023-04-30 13:10:27 -0300

    Atividade feita pelo professor

---
+++ src/StrategyDio/Comportamento.java
@@ -0,0 +1,5 @@
+package StrategyDio;
+
+public interface Comportamento {
+    void mover();
+}
---
+++ src/Teste.java
@@ -0,0 +1,50 @@
+import SingletonDio.SingletonEager;
+import SingletonDio.SingletonLazy;
+import SingletonDio.SingletonLazyHolder;
+import StrategyDio.*;
+
+public class Teste {
+    public static void main(String[] args) {
+        // Singleton
+
+        /*SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);*/
+
+        // Strategy
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // FacadeDio.Facade
+
+       /* FacadeDio.Facade facade = new FacadeDio.Facade();
+        facade.migrarCliente("Venilton", "14801788");*/
+
+
+
+    }
+}
